dadc635d30da8bf03103117603fee5c4107cf8d5
qandries <andries.quentin@gmail.com>
2018-05-09 10:52:19 +0200

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '280640319884-m6f9t3orrjuq0u79pn3e2ej91vg4f116.apps.googleusercontent.com',
+        clientSecret: 'yC2p42BYIlu-Zkv-XK78ZG62'
+    },
+    mongodb: {
+        dbURI: 'mongodb://netninja:test@ds117070.mlab.com:17070/nn-oauth-test'
+    },
+
+    session: {
+        cookieKey: 'thenetninjaisawesomeiguess'
+    }
+};
